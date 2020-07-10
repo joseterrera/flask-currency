c5f1f33ce84c4faa37a9cbe541451f3cf8934199
Jose Terrera <joseterrera@yahoo.com>
2020-07-09 22:26:49 -0700

    working on conversion route

--- currency-convertor/app.py
+++ currency-convertor/app.py
@@ -15,4 +15,7 @@ def submit_form():
   """Handle Form Submission"""
   code_from = request.args['code_from'].upper()
   code_to = request.args['code_to'].upper()
-  
+  amt = safe_convert_to_float(request.args['amt'])
+  errors = []
+
+  if amt is None:
